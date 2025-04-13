88eeea381b62f2599c8240fdc4f6c5540b934f93
nagasaimaddula <nagasaimaddula315@gmail.com>
2025-04-13 21:23:49 +0530

    deleted few files

--- src/test/java/starter/navigation/DuckDuckGoHomePage.java
+++
@@ -1,7 +0,0 @@
-package starter.navigation;
-
-import net.serenitybdd.annotations.DefaultUrl;
-import net.thucydides.core.pages.PageObject;
-
-public class DuckDuckGoHomePage extends PageObject {
-}
